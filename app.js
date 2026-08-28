80f40b22ff66dd6f0b40813153f16c2747639e58
Emanuele Favero <emafaux89@gmail.com>
2026-08-28 02:31:12 +0200

    feat: add /bacheca route

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import path from 'node:path';
 import express from 'express';
+import { posts } from './data/posts.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.get('/', (_req, res) => res.send('Server del mio blog'));
+
+app.get('/bacheca', (_req, res) => {
+  res.json(posts);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
