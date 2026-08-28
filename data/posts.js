38f5dc5958fb7fe04dbf90dc25cc9de50fa11876
Emanuele Favero <emafaux89@gmail.com>
2026-08-28 02:29:02 +0200

    feat: add data/posts array

---
+++ data/posts.js
@@ -0,0 +1,37 @@
+export const posts = [
+  {
+    id: 1,
+    title: 'Ciambellone della domenica',
+    content: 'Un ciambellone soffice e semplice, perfetto per la colazione.',
+    image: '/images/ciambellone.jpeg',
+    tags: ['dolci', 'colazione', 'ciambellone'],
+  },
+  {
+    id: 2,
+    title: 'Cracker rosa alla barbabietola',
+    content: 'Cracker croccanti alla barbabietola, ideali per uno snack.',
+    image: '/images/cracker-barbabietola.jpeg',
+    tags: ['cracker', 'barbabietola', 'snack'],
+  },
+  {
+    id: 3,
+    title: 'Pane fritto dolce',
+    content: 'Pane fritto dorato con una spolverata di zucchero.',
+    image: '/images/pane-fritto-dolce.jpeg',
+    tags: ['dolci', 'pane', 'tradizione'],
+  },
+  {
+    id: 4,
+    title: 'Pasta cremosa alla barbabietola',
+    content: 'Una pasta semplice con una cremosa salsa alla barbabietola.',
+    image: '/images/pasta-barbabietola.jpeg',
+    tags: ['pasta', 'barbabietola', 'primi'],
+  },
+  {
+    id: 5,
+    title: 'Torta paesana della tradizione',
+    content: 'La classica torta paesana, ricca e dal sapore rustico.',
+    image: '/images/torta-paesana.jpeg',
+    tags: ['dolci', 'torta', 'tradizione'],
+  },
+];
